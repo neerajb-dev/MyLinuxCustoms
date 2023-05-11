3c46f8b94fbe03ddf4a7e3cd9e27918bddceb9db
Neeraj Bhardwaj <neerajb@yahoo.com>
2023-05-11 15:37:02 +0530

    choices added to turn off/on night light while switching theme

--- .dark.sh
+++ .dark.sh
@@ -2,4 +2,10 @@
 gsettings set org.gnome.shell.ubuntu color-scheme prefer-dark
 gsettings set org.gnome.desktop.interface gtk-theme Yaru-dark
 gsettings set org.gnome.desktop.interface color-scheme prefer-dark 
-gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled true
+read -p "Do you want to turn on night light: y/n " choice
+if [ "$choice" = "y" ]; then
+        gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled true
+        echo "Switched to dark theme and turned on night light"
+else 
+        echo "Switched to dark theme" 
+fi
--- .light.sh
+++ .light.sh
@@ -2,4 +2,10 @@
 gsettings set org.gnome.shell.ubuntu color-scheme prefer-light
 gsettings set org.gnome.desktop.interface gtk-theme Yaru
 gsettings set org.gnome.desktop.interface color-scheme prefer-light
-gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled false
+read -p "Do you want to turn off night light: y/n " choice
+if [ "$choice" = "y" ]; then
+        gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled false
+        echo "Switched to light theme and turned of night light"
+else 
+        echo "Switched to light theme" 
+fi
